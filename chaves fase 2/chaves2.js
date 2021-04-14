ae98e72c59dad1b6de8157c4fb0660d6cfa76989
pedrobeltraoperalva@gmail.com <pedrobeltraoperalva@gmail.com>
2021-04-14 13:40:07 -0300

    erro de português

--- chaves fase 2/chaves2.js
+++ chaves fase 2/chaves2.js
@@ -11,10 +11,10 @@ function personagemChaves (){
         document.write(`
         <div class="texto">
         <p>Está tudo turvo, você vê uma fumaça verde, e escuta um grito muito alto: SATANÁÁÁÁÁÁÁÁS! Você cai para trás e 
-        depois do susto, você resolve ir até os fundos da vila, ver se tem mais alguém acordado para contar com 
-        muito medo sobre o que viu na casa da Bruxa do 71. Chegando nos fundos da vila você encontra o kiko de costas
+        depois do susto, resolve ir até os fundos da vila ver se tem mais alguém acordado para contar, com 
+        muito medo, sobre o que viu na casa da Bruxa do 71. Chegando nos fundos da vila você encontra o kiko de costas
         , se mexendo de forma estranha, tremendo um pouco. Você encosta a mão no ombro dele e quando percebe ele está 
-        segurando um facão. Logo ele se vira para você e perceber que é o Jason vestido de kiko! O que você vai fazer?</p>
+        segurando um facão. Logo ele se vira para você e percebe que é o Jason vestido de kiko! O que você vai fazer?</p>
         <ul>
         <li>1- Perguntar se o Jason quer brincar?</li>
         <li>2- Sair correndo dali?</li>
